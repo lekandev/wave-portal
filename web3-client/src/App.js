6869d207c02b393e622d110260a1d6402060816c
Mohammed Kabir Hussaini <65419835+lekandev@users.noreply.github.com>
2021-11-24 01:16:05 +0100

    web3-client

---
+++ web3-client/src/App.js
@@ -0,0 +1,29 @@
+import * as React from "react";
+import { ethers } from "ethers";
+import './App.css';
+
+export default function App() {
+
+  const wave = () => {
+    
+  }
+  
+  return (
+    <div className="mainContainer">
+
+      <div className="dataContainer">
+        <div className="header">
+        👋 Hey there!
+        </div>
+
+        <div className="bio">
+        I am lekandev and I am a fullstack and mobile developer Connect your Ethereum wallet and wave at me!
+        </div>
+
+        <button className="waveButton" onClick={wave}>
+          Wave at Me
+        </button>
+      </div>
+    </div>
+  );
+}
