516bbe0b167aa422d313f4a86d3628a1e94b32f1
Mohammed Kabir Hussaini <65419835+lekandev@users.noreply.github.com>
2021-11-24 02:51:21 +0100

    sum

--- web3-client/src/App.js
+++ web3-client/src/App.js
@@ -1,49 +1,92 @@
-import * as React from "react";
+import React, { useEffect, useState } from "react";
 // import { ethers } from "ethers";
-import './App.css';
+import "./App.css";
 
 export default function App() {
+  /*
+   * Just a state variable we use to store our user's public wallet.
+   */
+  const [currentAccount, setCurrentAccount] = useState("");
 
-  const checkIfWalletIsConnected = () => {
-    /*
-     * First make sure we have access to window.ethereum
-     */
-    const { ethereum } = window;
+  const checkIfWalletIsConnected = async () => {
+    try {
+      const { ethereum } = window;
 
-    if (!ethereum) {
-      console.log("Make sure you have metamask!");
-      return;
-    } else {
-      console.log("We have the ethereum object", ethereum);
+      if (!ethereum) {
+        console.log("Make sure you have metamask!");
+        return;
+      } else {
+        console.log("We have the ethereum object", ethereum);
+      }
+
+      /*
+       * Check if we're authorized to access the user's wallet
+       */
+      const accounts = await ethereum.request({ method: "eth_accounts" });
+
+      if (accounts.length !== 0) {
+        const account = accounts[0];
+        console.log("Found an authorized account:", account);
+        setCurrentAccount(account);
+      } else {
+        console.log("No authorized account found");
+      }
+    } catch (error) {
+      console.log(error);
     }
   };
 
-  /*
-   * This runs our function when the page loads.
+  /**
+   * Implement your connectWallet method here
    */
+  const connectWallet = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        alert("Get MetaMask!");
+        return;
+      }
+
+      const accounts = await ethereum.request({
+        method: "eth_requestAccounts",
+      });
+
+      console.log("Connected", accounts[0]);
+      setCurrentAccount(accounts[0]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   useEffect(() => {
     checkIfWalletIsConnected();
   }, []);
 
-  const wave = () => {
-    
-  }
-  
+  const wave = () => {};
+
   return (
     <div className="mainContainer">
-
       <div className="dataContainer">
-        <div className="header">
-        Hey there!
-        </div>
+        <div className="header">Hey there!</div>
 
         <div className="bio">
-        I am lekandev and I am a fullstack and mobile developer Connect your Ethereum wallet and wave at me!
+          I am lekandev and I am a fullstack and mobile developer Connect your
+          Ethereum wallet and wave at me!
         </div>
 
         <button className="waveButton" onClick={wave}>
           Wave at Me
         </button>
+
+        {/*
+         * If there is no currentAccount render this button
+         */}
+        {!currentAccount && (
+          <button className="waveButton" onClick={connectWallet}>
+            Connect Wallet
+          </button>
+        )}
       </div>
     </div>
   );
