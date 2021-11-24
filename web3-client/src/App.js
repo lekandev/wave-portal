e85129b3d6c48627a70cb068a420dfb6f01bbd6b
Mohammed Kabir Hussaini <65419835+lekandev@users.noreply.github.com>
2021-11-24 02:39:22 +0100

    Update App.js

--- web3-client/src/App.js
+++ web3-client/src/App.js
@@ -4,6 +4,27 @@ import './App.css';
 
 export default function App() {
 
+  const checkIfWalletIsConnected = () => {
+    /*
+     * First make sure we have access to window.ethereum
+     */
+    const { ethereum } = window;
+
+    if (!ethereum) {
+      console.log("Make sure you have metamask!");
+      return;
+    } else {
+      console.log("We have the ethereum object", ethereum);
+    }
+  };
+
+  /*
+   * This runs our function when the page loads.
+   */
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, []);
+
   const wave = () => {
     
   }
