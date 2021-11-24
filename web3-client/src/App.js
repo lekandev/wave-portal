fdc48c032c1b96982d2346f92bf76e6acc0e4a8b
Mohammed Kabir Hussaini <65419835+lekandev@users.noreply.github.com>
2021-11-24 12:47:13 +0100

    Update App.js

--- web3-client/src/App.js
+++ web3-client/src/App.js
@@ -17,7 +17,7 @@ export default function App() {
 
   useEffect(() => {
     checkIfWalletIsConnected();
-  }, []);
+  }, [checkIfWalletIsConnected]);
 
   return (
     <div className="mainContainer">
@@ -29,7 +29,7 @@ export default function App() {
             <img src="/assets/profile.jpg" alt="profile" class="profilePicture" />
           </div>
 
-          <a href="https://lekan.vercel.app" target="_blank" rel="noopener" className="portfolioButton">Check out my portfolio</a>
+          <a href="https://lekan.vercel.app" target="_blank" rel="noopener norefferer" className="portfolioButton">Check out my portfolio</a>
         </div>
 
         <div className="bio">
